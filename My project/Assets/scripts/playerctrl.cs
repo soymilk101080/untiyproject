2b34794450c3a32835aee5e5b33000ac85effc45
1131415023 <1131415023@mail.hwu.edu.tw>
2025-10-08 16:47:19 +0800

    移動

--- My project/Assets/scripts/playerctrl.cs
+++ My project/Assets/scripts/playerctrl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerCtrl: MonoBehaviour
 {
+    public CharacterController charCtrl;
     public int HP;
 
     //初始化
@@ -15,6 +16,7 @@ public class PlayerCtrl: MonoBehaviour
     //更新:偵測操作
     void Update()
     {
-        
+        //當(輸入按鍵w壓下)角色控制器.移動(往前)
+        if (Input.GetKey(KeyCode.W)) charCtrl.SimpleMove(Vector3.forward);
     }
 }
