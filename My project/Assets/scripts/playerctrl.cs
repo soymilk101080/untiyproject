17d7980d60f960f8923c4e66c56edc2ac4df4e65
1131415023 <1131415023@mail.hwu.edu.tw>
2025-11-19 16:48:57 +0800

    jump

--- My project/Assets/scripts/playerctrl.cs
+++ My project/Assets/scripts/playerctrl.cs
@@ -15,6 +15,10 @@ public class PlayerCtrl: MonoBehaviour
     /// </summary>
     public float moveSpeed = 3f;
     /// <summary>
+    /// 角色跳躍高度
+    /// </summary>
+    public float jumpHeight = 2f;
+    /// <summary>
     /// 角色的血量
     /// </summary>
     public int HP;
@@ -23,6 +27,7 @@ public class PlayerCtrl: MonoBehaviour
     /// </summary>
     Vector2 input;
     Vector3 look;
+    float speedV;
     #endregion 基本參數
 
     #region 角色公開狀態
@@ -30,6 +35,18 @@ public class PlayerCtrl: MonoBehaviour
     /// 角色是否有接收輸入操作
     /// </summary>
     public bool isMove => input != Vector2.zero;
+    /// <summary>
+    /// 是否接觸地面
+    /// </summary>
+    public bool isGrounded => charCtrl.isGrounded;
+    /// <summary>
+    /// \重力常數
+    /// </summary>
+    public float G => 9.8f;
+    /// <summary>
+    /// 角色動能包含:水平和垂直重力
+    /// </summary>
+    public Vector3 Velocity => transform.forward * moveSpeed * input.magnitude + Vector3.up * speedV;
     #endregion 角色公開狀態
 
     #region UNITY生命週期
@@ -48,6 +65,7 @@ public class PlayerCtrl: MonoBehaviour
     {
         Action();
         animator.SetBool("IsMove",isMove);
+        animator.SetFloat("BlendInput", input.magnitude);
     }
     #endregion UNITY生命週期
 
@@ -60,20 +78,27 @@ public class PlayerCtrl: MonoBehaviour
         //轉動角色
         look.z = input.y;
         look.x = input.x;
-
-        //有移動操作產生時
-        if (isMove)
-        {
-            //角色控制框轉向操作方向
-            transform.rotation = Quaternion.LookRotation(look);
-            //角色控制器.移動(往前)
-            charCtrl.SimpleMove(transform.forward * moveSpeed);
-        }
+        //角色控制框轉向操作方向
+        if (!isMove)transform.rotation = Quaternion.LookRotation(look);
+        //角色控制器.移動(往前)+地心引力(標準化 1/FPS)
+        speedV -= G * Time.deltaTime;
+        charCtrl.Move(Velocity * Time.deltaTime);
     }
 
     public void Move(CallbackContext callback) 
     {
         input = callback.ReadValue<Vector2>();
+        Debug.Log(input);
+    }
+
+    public void Jump(CallbackContext callback)
+    {
+        //從地面起跳
+        if (isGrounded && callback.performed)
+        {
+            Debug.Log("從地面起跳");
+            speedV = Mathf.Sqrt(2 * jumpHeight * G);
+        }
     }
     #endregion 操作設計
 }
